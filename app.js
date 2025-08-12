a0c567057b268b149f29deb32d297031e2c98fb8
YURI SILVA DE OLIVEIRA <25132703@CFP127ALUNO.LOCAL>
2025-08-12 16:34:07 -0300

    semi-finalizado

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict'
+
+import produtos from './funcionarios.json' with {type: 'json'}
+
+    function mostrarPreços (produto){
+        console.log(produto.categoria(0))
+    }
+
+
+produtos.forEach (mostrarPreços)
